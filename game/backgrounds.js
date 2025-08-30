9d97993444753cfb80071b4a05f1acad7fed763f
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-08-30 10:26:00 -0500

    Added stuff O(∩_∩)O

---
+++ game/backgrounds.js
@@ -0,0 +1,18 @@
+export class Grass{
+    constructor(game){
+        this.game = game;
+        this.grassImg = document.getElementById("grass");
+        this.maxFrameX = 25;
+        this.maxFrameY = 14;
+        this.spriteWidth = 400/this.maxFrameX;
+        this.spriteHeight = 224/this.maxFrameY;
+        //dont need frameX or frameY since it is a background. Not a animation/spritesheet
+        this.season = this.game.season;
+    }
+    update(dt){
+        this.season = this.game.season;
+    }
+    draw(ctx){
+        
+    }   
+}
