65df4802734cbd0d0bd4007eba9a8caf318bae0f
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-09-01 10:37:24 -0500

    Background for timer

--- datetime/script.js
+++ datetime/script.js
@@ -17,28 +17,40 @@ function getMonthName(m){
     switch(m){
         case 0:
             return "January";
+            break;
         case 1:
             return "February";
+            break;
         case 2:
             return "March";
+            break;
         case 3:
             return "April";
+            break;
         case 4:
             return "May";
+            break;
         case 5:
             return "June";
+            break;
         case 6:
             return "July";
+            break;
         case 7:
             return "August";
+            break;
         case 8:
             return "September";
+            break;
         case 9:
             return "October";
+            break;
         case 10:
             return "November";
+            break;
         case 11:
             return "December";
+            break;
         default:
             return;
     }
@@ -58,4 +70,5 @@ let currentMinute = currentDateAndTime.getMinutes();
 let currentSecond = currentDateAndTime.getSeconds();
 let date = currentMonthName + " " + currentDate + " " + currentYear;
 let time = `${currentHour}:${currentMinute}:${currentSecond}`;
+
 setInterval(Update, 1000);
