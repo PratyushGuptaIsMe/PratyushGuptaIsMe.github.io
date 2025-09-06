5830bbcb1fc9b08648b28685fe9c7138b715dda7
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-09-05 22:11:21 -0500

    Image overhaul, style changes, new functions script, NEW FILLERTEXT

---
+++ main-functions.js
@@ -0,0 +1,11 @@
+async function insertRandomImgToHTMLImgTag(id){
+    document.getElementById(id).src = await "https://picsum.photos/300/300";
+    document.getElementById(id).style.width = "300px";
+    document.getElementById(id).style.height = "300px";
+}
+
+function insertImgToTag(id, path, w, h){
+    document.getElementById(id).src = path;
+    document.getElementById(id).style.width = w;
+    document.getElementById(id).style.height = h;
+}
