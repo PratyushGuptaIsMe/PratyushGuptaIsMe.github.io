5181782a98fdae213f3019f8f7cac6c96f80bcef
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-10-11 10:34:47 -0500

    100th commit!!!!!! Added Offline handling

---
+++ main.js
@@ -0,0 +1,10 @@
+const LINKCOLLECTION = document.getElementsByClassName("link");
+const LINKS = Array.from(LINKCOLLECTION);
+LINKS.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        if(navigator.onLine === false){
+            e.preventDefault();
+            window.location.href = "offline.html";
+        }
+    })
+})
--- preloaded.js
+++ preloaded.js
@@ -1,2 +1,11 @@
-let online = navigator.onLine;  //bool onlineornot
-animation_loop();
+async function insertRandomImgToHTMLImgTag(id){
+    document.getElementById(id).src = await "https://picsum.photos/300/300";
+    document.getElementById(id).style.width = "300px";
+    document.getElementById(id).style.height = "300px";
+}
+
+function insertImgToTag(id, path, w, h){
+    document.getElementById(id).src = path;
+    document.getElementById(id).style.width = w;
+    document.getElementById(id).style.height = h;
+}
